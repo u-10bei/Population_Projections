84efa4125fede1c25fec8cce1b69b14e256a3155
u-10bei <holy19690206@gmail.com>
2022-06-26 16:34:26 +0900

    add R_VAR_1

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 # Ｒによる推計
 | モデル | ＡＲＩＭＡ | ＶＡＲ | ＧＡＲＣＨ | 状態空間 |
 | :---: | :---: | :---: | :---: | :---: |
-| 総人口だけ使用 | [Ｒ_人口推計_その１](https://colab.research.google.com/drive/11iBGtzEXfOynuyhiYJIXVgo-OojV0B0G?usp=sharing) ||||
-| 人口方程式の導入 | [Ｒ_人口推計_その２](https://colab.research.google.com/drive/1EF1ngKafPHpPy1QcZHBZ_8wmvGunLWbK?usp=sharing) ||||
-| 出生数と死亡率 | [Ｒ_人口推計_その３](https://colab.research.google.com/drive/1MZ2XYpcnUZ8Dg7uYU-BEqRQOPy0y7u8r?usp=sharing) ||||
-| 年齢３区分ごとの死亡率 | [Ｒ_人口推計_その４](https://colab.research.google.com/drive/1QphFr9OsyonZQlRDjsa0rYdg8BUGI3N9?usp=sharing) ||||
+| 総人口だけ使用 | [ R_ARIMA_1 ](https://colab.research.google.com/drive/11iBGtzEXfOynuyhiYJIXVgo-OojV0B0G?usp=sharing) | [ R_VAR_1 ](https://colab.research.google.com/drive/1orXQ4QF-KDg9mpTKYocHMFMBkZL1_cms?usp=sharing) |||
+| 人口方程式の導入 | [ R_ARIMA_2 ](https://colab.research.google.com/drive/1EF1ngKafPHpPy1QcZHBZ_8wmvGunLWbK?usp=sharing) ||||
+| 出生数と死亡率 | [ R_ARIMA_3 ](https://colab.research.google.com/drive/1MZ2XYpcnUZ8Dg7uYU-BEqRQOPy0y7u8r?usp=sharing) ||||
+| 年齢３区分ごとの死亡率 | [ R_ARIMA_4 ](https://colab.research.google.com/drive/1QphFr9OsyonZQlRDjsa0rYdg8BUGI3N9?usp=sharing) ||||
 
 # Ｐｙｔｈｏｎによる推計
 | モデル | ＡＲＩＭＡ | ＶＡＲ | ＧＡＲＣＨ | 状態空間 |
 | :---: | :---: | :---: | :---: | :---: |
-| 総人口だけ使用 | [Ｐｙｔｈｏｎ_人口推計_その１](https://colab.research.google.com/drive/1--2tl9CaoS7YWa9YM9-K7kTxgimLVrVx?usp=sharing) ||||
-| 人口方程式の導入 | [Ｐｙｔｈｏｎ_人口推計_その２](https://colab.research.google.com/drive/1Hqauyd81PvWexKfJvrkYFm8YTfW4MplS?usp=sharing) ||||
-| 出生数と死亡率 | [Ｐｙｔｈｏｎ_人口推計_その３](https://colab.research.google.com/drive/14Ypt-UupLji-O6UhD5rn4-spJzyB9CT3?usp=sharing) ||||
-| 年齢３区分ごとの死亡率 | [Ｐｙｔｈｏｎ_人口推計_その４](https://colab.research.google.com/drive/1lgXdk2FnCO9Onn05Yje_xcpaJSoJQC3J?usp=sharing) ||||
+| 総人口だけ使用 | [ Python_ARIMA_1 ](https://colab.research.google.com/drive/1--2tl9CaoS7YWa9YM9-K7kTxgimLVrVx?usp=sharing) ||||
+| 人口方程式の導入 | [ Python_ARIMA_2 ](https://colab.research.google.com/drive/1Hqauyd81PvWexKfJvrkYFm8YTfW4MplS?usp=sharing) ||||
+| 出生数と死亡率 | [ Python_ARIMA_3 ](https://colab.research.google.com/drive/14Ypt-UupLji-O6UhD5rn4-spJzyB9CT3?usp=sharing) ||||
+| 年齢３区分ごとの死亡率 | [ Python_ARIMA_4 ](https://colab.research.google.com/drive/1lgXdk2FnCO9Onn05Yje_xcpaJSoJQC3J?usp=sharing) ||||
---
+++ Rcode/VAR1.R
@@ -0,0 +1,111 @@
+#ライブラリインストール(必要に応じて)
+#install.packages( "tidyverse" )
+#install.packages( "fable" )
+#install.packages( "feasts" )
+#install.packages( "urca" )
+#install.packages( "reshape2" )
+
+# 該当リポジトリを変数に格納
+c( "https://raw.githubusercontent.com/u-10bei/Population_Projections/" ) -> repo
+# 該当ＵＲＬを変数に格納
+repo |> paste0( c( "main/data/population_jp_year.csv" )) -> popURL
+
+# ライブラリの読み込み
+library( readr )
+library( fable )
+library( dplyr )
+
+# ネット上のファイル読み込み
+popURL |>
+  read_csv( show_col_types = FALSE ) |>
+  # ＴＳＩＢＢＬＥライブラリに変換
+  select( Year, Total ) |>
+  as_tsibble( index = Year ) -> pop_tsibble
+
+# ライブラリの読み込み
+library( ggplot2 )
+
+# 総人口のグラフ
+pop_tsibble |>
+  autoplot( Total )
+
+# ライブラリの読み込み
+library( feasts )
+
+# 自己相関のグラフ
+pop_tsibble |>
+  ACF( Total ) |>
+  autoplot()
+
+# 偏自己相関のグラフ
+pop_tsibble |>
+  PACF( Total ) |>
+  autoplot()
+
+pop_tsibble |>
+  model(STL( Total ~ season( window = Inf ))) |>
+  components() |>
+  autoplot()
+
+# 学習データと予測データ
+5 -> prow_test
+pop_tsibble |> nrow() - prow_test -> prow_train
+pop_tsibble |> tail( n = prow_test ) -> pop_test
+pop_tsibble |> head( n = prow_train ) -> pop_train
+
+# ＶＡＲモデルの推定
+pop_train |>
+  model( var = VAR( Total, ic = "aic" )) -> pop_var
+
+# ＶＡＲによる予測
+pop_var |>
+forecast( xreg = pop_test$Total,
+          h = "5 years") -> pop_var_f
+
+# 社人研予測との比較
+# 該当ＵＲＬを変数に格納
+repo |> paste0( c( "main/data/forecast_ipss.csv" )) -> ipssURL
+                
+# ネット上のファイル読み込み
+ipssURL |>
+  read_csv( show_col_types = FALSE ) |>
+  # ＴＳＩＢＢＬＥライブラリに変換
+  as_tsibble( index = Year ) -> ipss_test
+
+pop_var_f |>
+  as.data.frame() |>
+  select( Year, "forecast" = .mean ) |> 
+  inner_join( pop_test, by = "Year" ) |>
+  inner_join( ipss_test, by = "Year" ) |>
+  select( Year,
+          Total,
+          forecast,
+          DMBM,
+          DMBH,
+          DLBM,
+          DLBH ) -> join_test
+join_test
+
+# ライブラリの読み込み
+library( reshape2 )
+
+join_test |> 
+  melt(id="Year",measure=c( "Total",
+                            "forecast",
+                            "DMBM",
+                            "DMBH",
+                            "DLBM",
+                            "DLBH")) -> join_plot
+
+#描画
+ggplot( join_plot,
+        aes(x = Year,
+            y = value,
+            shape = variable,
+            colour = variable,
+            group = variable )) +
+  geom_line() +
+  geom_point()
+
+pop_arima_f |> autoplot() +
+  autolayer( pop_test )
